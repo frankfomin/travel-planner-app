134ccc554dbbffd1d6b773ebb34b063e5e7f6c2f
FronkDonk <124029266+FronkDonk@users.noreply.github.com>
2023-09-01 15:57:26 +0200

    5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
     // Set a delay of 300 milliseconds (adjust as needed)
     const delay = setTimeout(() => {
       setDelayedValue(value);
-    }, 400);
+    }, 300);
 
     // Clear the timeout if the value changes before the delay
     return () => clearTimeout(delay);
@@ -74,7 +74,7 @@ export default function Home() {
                     key={i}
                     className="flex justify-between items-center hover:cursor-pointer p-2 hover:bg-slate-900"
                   >
-                    <Skeleton className="w-[80%] h-[15px]   rounded-full" />
+                    <Skeleton className="w-[80%] h-[15px] rounded-full" />
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
@@ -85,7 +85,7 @@ export default function Home() {
                       strokeWidth="2"
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      className="feather feather-map-pin"
+                      className="feather feather-map-pin "
                     >
                       <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
                       <circle cx="12" cy="10" r="3"></circle>
