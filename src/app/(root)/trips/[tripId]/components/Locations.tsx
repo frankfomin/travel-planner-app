7fb3a46869feeb5a706bd76824804d249debc367
FronkDonk <124029266+FronkDonk@users.noreply.github.com>
2023-12-12 00:28:40 +0100

    a

--- src/app/(root)/trips/[tripId]/components/Locations.tsx
+++ src/app/(root)/trips/[tripId]/components/Locations.tsx
@@ -1,5 +1,4 @@
 import LocationCard from "@/app/(root)/your-trip/components/LocationCard";
-import { options } from "@/app/api/auth/[...nextauth]/options";
 import { Place } from "@/types";
 import axios from "axios";
 import { headers } from "next/headers";
