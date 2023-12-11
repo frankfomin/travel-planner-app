72b90419336a17ea52b94293c8dd68b49e3cbadf
FronkDonk <124029266+FronkDonk@users.noreply.github.com>
2023-12-12 00:07:05 +0100

    yup

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,5 @@
-import { headers } from "next/headers";
 import Wrapper from "./components/Wrapper";
 
 export default function HomePage() {
-  const header = headers();
-  const ip = header.get("x-forwarded-for")!;
-
-  console.log("IP", ip);
   return <Wrapper />;
 }
