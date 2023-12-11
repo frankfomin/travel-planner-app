8066fdf23c5e1f188a1f24184fbd58da32ddadaf
FronkDonk <124029266+FronkDonk@users.noreply.github.com>
2023-12-11 23:36:14 +0100

    FUCKING PLEASE

--- src/app/(root)/(profile)/profile/account/components/UserFormData.tsx
+++ src/app/(root)/(profile)/profile/account/components/UserFormData.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { TupdateUserData, updateUserData } from "@/lib/types";
+import { TupdateUserData, updateUserData } from "@/lib/validators/updateUser";
 import {
   Form,
   FormControl,
