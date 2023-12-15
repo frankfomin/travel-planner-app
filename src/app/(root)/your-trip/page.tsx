d6d2dff87e0aa2e0812540fc429927cd891132d3
FronkDonk <124029266+FronkDonk@users.noreply.github.com>
2023-12-15 18:17:21 +0100

    force dynamic again

--- src/app/(root)/your-trip/page.tsx
+++ src/app/(root)/your-trip/page.tsx
@@ -3,6 +3,8 @@ import YourTrip from "./components/YourTrip";
 import { Suspense } from "react";
 import Loading from "./Loading";
 
+export const dynamic = "force-dynamic";
+
 export default async function YourTripPage() {
   return (
     <main>
