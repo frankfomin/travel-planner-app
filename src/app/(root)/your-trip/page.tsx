f58bb199bd15c2af4ec4b1107c16c6d79bc7be54
FronkDonk <124029266+FronkDonk@users.noreply.github.com>
2024-01-16 17:58:06 +0100

    runtime edge trip page

--- src/app/(root)/your-trip/page.tsx
+++ src/app/(root)/your-trip/page.tsx
@@ -5,6 +5,8 @@ import { location, trip } from "@/db/schema";
 import { nanoid } from "nanoid";
 import Loading from "@/components/loading/Loading";
 
+export const runtime = "edge";
+
 export default async function YourTripPage() {
   return (
     <main className="p-3">
